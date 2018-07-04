b1a62b9af58ada287d966ede48177294230b17a2
umran <umran.aras@sekomyazilim.com.tr>
2018-07-04 10:55:02 +0300

    added author name

--- src/BeginnerToJava.java
+++ src/BeginnerToJava.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class BeginnerToJava {
     public static void main(String[] args) {
+
+        // author arasumran
         int return_counter =99999999;
         int [] A ={1,6,2,3,3,2,4,5,4,3,2,1,2,3,4,5,6,5,4,3,2,1,1,1,2,3,4,5,5,4,3,2,3,34,5,5,5,5,6,6,6,4,3,4,3,4,2,3,1};
 
@@ -38,13 +40,6 @@ public class BeginnerToJava {
 
 
 
-       /*
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();int [] list = {1,2,4,5,6,7,8,9};
-        for(int i =0;i<list.length;i++){arrayList.add(list[i]); // System.out.println("" + i +".deneme "+ " : "+ var);}
-        //System.out.println("max_value : " + max_value +"\n"+ "min_value : " + min_value);
-        int smallest_value =1;for (Integer var :arrayList){if (var.equals(smallest_value)){smallest_value +=1;}}System.out.println(smallest_value);
-
-*/
         }
 
     }
